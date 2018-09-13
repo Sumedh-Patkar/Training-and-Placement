bd1b6129a23324aa9e930fb7d20f8504cde24e5c
Sumedh Patkar <sumedh.patkar19@gmail.com>
2018-09-13 14:47:44 +0530

    Modified Classes

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,10 +1,22 @@
 package application;
 
-public class Main {
+import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.control.Hyperlink;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
 
+public class Main extends Application{
+
+	@Override
+    public void start(Stage stage) {
+        
+    }
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
 	}
 
 }
